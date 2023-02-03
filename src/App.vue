4503c755b268248a41008b940c72656a974542df
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-03 20:45:53 +0600

    Done my first vue project

--- src/App.vue
+++ src/App.vue
@@ -45,4 +45,20 @@ export default {
   color: #444;
   margin-top: 60px;
 }
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+  letter-spacing: 1px;
+  margin: 10px;
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
 </style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,11 +1,39 @@
 <template>
   <p>Reaction Time: {{ score }} ms</p>
+  <div class="rank">
+    {{ rank }}
+  </div>
 </template>
 
 <script>
 export default {
   props: ["score"],
+
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Master";
+    } else if (this.score < 400) {
+      this.rank = "Expert";
+    } else if (this.score < 700) {
+      this.rank = "Average";
+    } else if (this.score < 1000) {
+      this.rank = "Beginner";
+    } else {
+      this.rank = "Newbie";
+    }
+  },
 };
 </script>
 
-<style></style>
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
