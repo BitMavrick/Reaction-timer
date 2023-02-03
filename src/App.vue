cc36f003012ffde0de09f53cb0d9a410f7b20531
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-03 17:07:55 +0600

    Lifecycle hook

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from "./components/Block.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
 };
 </script>
 
---
+++ src/components/Block.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="block" v-if="showBlock">Click Me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      console.log(this.delay);
+    }, this.delay);
+  },
+  updated() {
+    console.log("component updated");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
